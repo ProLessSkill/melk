61164d5b657975a1b11ab2e0611d29d3846cbcec
ProLessSkill <42057840+ProLessSkill@users.noreply.github.com>
2018-10-12 13:42:55 +0200

    Add files via upload

---
+++ melkbot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '-')
+Clientdiscord = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='      '))
+    print('Ready :D') 
+
+
+@client.event
+async def on_message(message):
+    if message.content == '-ping':
+        await client.send_message(message.channel,'pooooooooooooooooooooooooooooooooooong.')
+client.run('NDg2NDg1MzAxMzEzNjY3MDgy.DppcTQ.T4TVwlLRt6S-8VcJ2Vxfqntmil0')
