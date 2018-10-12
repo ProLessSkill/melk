35d9305e3c7168fce5544ab6eacc067b5b10eb68
ProLessSkill <42057840+ProLessSkill@users.noreply.github.com>
2018-10-12 19:49:39 +0200

    Update melkbot.py

--- melkbot.py
+++ melkbot.py
@@ -31,4 +31,6 @@ async def on_message(message):
         em = discord.Embed(description='Keep your friends close, but your enemies closer.')
         em.set_image(url='https://www.google.bg/search?q=nature+images&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiqvbCrnYHeAhUCjywKHffxD8oQ_AUIDigB&biw=1920&bih=938#imgrc=zTqmwGzV3Q_bIM')
         await client.send_message(message.channel, embed=em)
+    if message.content == '-invite':
+        await client.send_message(message.channel,'Hey big bois! Im Melk, your favorite bot. Now invite me to your server to not be like a retard. Im being updated very frequently and with 24/7 uptime. Inv. Link: ')
 client.run('NDg2NDg1MzAxMzEzNjY3MDgy.DqJMXg.IZ4bShqdoi7IbMK9DNlrk9RqN2o')
