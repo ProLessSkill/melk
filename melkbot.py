c8eec42afa3e7638c9e6e54306ec8a434ff0addc
ProLessSkill <42057840+ProLessSkill@users.noreply.github.com>
2018-10-23 15:57:49 +0300

    Update melkbot.py

--- melkbot.py
+++ melkbot.py
@@ -14,24 +14,24 @@ Clientdiscord = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='despacito with Melk'))
+    await client.change_presence(game=Game(name='with the community :)'))
     print('Ready, Freddy') 
 
 
 @client.event
 async def on_message(message):
     if message.content == '-ping':
-        await client.send_message(message.channel,'Pong! Im online, playing despacito!')
+        await client.send_message(message.channel,'Pong!')
     if message.content == '-credits':
         await client.send_message(message.channel,'**Contributors:** `ProLessSkill#7393 - Owner`')
     if message.content.startswith('-DidIDrinkEnoughMelk'):
         randomlist = ['no. you need to drink more!','sure i guess','you did not drink at all.','just a little bit more...',]
         await client.send_message(message.channel,(random.choice(randomlist)))
-    if message.content == '-SongOfTheWeek':
+    if message.content == '-abshajsbsushsgs':
         em = discord.Embed(description='SOTW: https://www.youtube.com/watch?v=kJQP7kiw5Fk | You may request songs at https://docs.google.com/forms/d/1ZSzFIOWy4c0yw7jDq_YqB-ED5AJMIyDN-HQVYnSpFBo | Dont mind the cringe')
         em.set_image(url='https://www.google.bg/search?q=nature+images&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiqvbCrnYHeAhUCjywKHffxD8oQ_AUIDigB&biw=1920&bih=938#imgrc=zTqmwGzV3Q_bIM')
         await client.send_message(message.channel, embed=em)
-    if message.content == '-invite':
+    if message.content == '-inviteMelk':
         await client.send_message(message.channel,'Hey big bois! Im Melk, your favorite bot. Now invite me to your server to not be like a retard. Im being updated very frequently and with 24/7 uptime. Inv. Link: ')
     if message.content == '-serverping':
         await client.send_message(message.channel,'❗ Specify a Server! Eg: ```Europe | USA```')
